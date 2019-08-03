268fe0ad1abe8c2a3f7ffdd373bb263e9b0f0b43
AS Wilson <alexander.wilson@ncf.edu>
2019-08-03 12:33:16 -0500

    Init basic server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-//restify here!
+const restify = require('restify');
+
+const server = restify.createServer({
+  name: 'Strictly Bikes',
+  version: '1.0.0'
+});
+
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
+
+server.get('/', (req, res) => {
+  res.send('~Strictly Bikes~');
+});
+
+server.listen(3000, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
