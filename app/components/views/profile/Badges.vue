ff86a3c78ac5f5d355f877dccb7ebcde0b8f6f99
makiyah cormick <retrofied23@gmail.com>
2019-08-03 11:18:02 -0500

    Set up file structure and imported font-awesome

---
+++ app/components/views/profile/Badges.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      name() {
+        
+      }
+    },
+    data() {
+      return {
+        key: value
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- app/main.js
+++ app/main.js
@@ -1,8 +1,20 @@
 import Vue from 'nativescript-vue'
-import App from './components/App'
+import Home from './components/views/Home'
 import store from './store';
 
 import VueDevtools from 'nativescript-vue-devtools'
+//import for the favicons
+import {TNSFontIcon, fonticon} from 'nativescript-fonticon';
+
+TNSFontIcon.debug = true;
+TNSFontIcon.paths = {
+  'fa': './font-awesome.css',
+  'ion': './ionicons.css'
+};
+TNSFontIcon.loadCss(); 
+
+Vue.filter('fonticon', fonticon);
+// end of favicon code
 
 if(TNS_ENV !== 'production') {
   Vue.use(VueDevtools)
@@ -10,8 +22,7 @@ if(TNS_ENV !== 'production') {
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
 
-
 new Vue({
   store,
-  render: h => h('frame', [h(App)])
+  render: h => h('frame', [h(Home)])
 }).$start()
