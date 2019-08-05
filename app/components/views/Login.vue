25e842439e2dcdd665a44c093cfdd24c8654dd2d
AS Wilson <alexander.wilson@ncf.edu>
2019-08-04 22:04:43 -0500

    Implement Login onTap

--- app/components/views/Login.vue
+++ app/components/views/Login.vue
@@ -3,22 +3,23 @@
     <ActionBar title="Stricly Bikes"/>
         <ScrollView>
           <StackLayout orientation="vertical" width="*" height="*" backgroundColor="lightgray">
-            <Button text="Button" @tap="onLoginClick" height="10%"/>
+            <Button text="Button" @tap="onLoginTap" height="10%"/>
           </StackLayout>
         </ScrollView>
   </Page>
 </template>
 
 <script>
+  import { tnsOauthLogin } from '../../auth/auth-service';
   export default {
     methods: {
-      onLoginClick() {
+      onLoginTap() {
         console.log("login clicked");
+        tnsOauthLogin('google');
       }
     },
     data() {
       return {
-        key: value
       }
     },
   }
