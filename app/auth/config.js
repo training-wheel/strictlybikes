9f277f7bee9672e66d5d59644f0535b2103a2d53
Darrian Brown <Darrian Brown>
2019-08-05 19:39:46 -0500

    finished logging To, From and Condition values. Moving on to tom tom api

---
+++ app/auth/config.js
@@ -0,0 +1,3 @@
+export const WEB_CLIENT_ID = '1087785445782-5lra79gf5nkr8ellmd4kjbe97lmbbbdd.apps.googleusercontent.com';
+export const WEB_REDIRECT_URI = 'https://6a8ade58.ngrok.io/auth/google/callback';
+export const WEB_URL_SCHEME = '	https://6a8ade58.ngrok.io';
