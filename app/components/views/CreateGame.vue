d52502e1b263fc778005719617990d7eaf6c7949
Darrian Brown <Darrian Brown>
2019-08-08 13:54:55 -0500

    Added updated templates to views Added 4 markers to current mapbox view in createGame Changed button names and layout

---
+++ app/components/views/CreateGame.vue
@@ -0,0 +1,128 @@
+<template>
+    <Page class="page">
+        <ActionBar class="action-bar" title="Create Game"></ActionBar>
+        <StackLayout>
+                <Mapbox
+                    accessToken="sk.eyJ1Ijoic3RyaWN0bHliaWtlcyIsImEiOiJjanoxZ3dsMXUwMGthM29udDZyYmR1azkzIn0.dTFjDdkaX0N-YgfOgLLoOQ"
+                    mapStyle="traffic_day"
+                    latitude="29.9643504"
+                    longitude="-90.0816426"
+                    hideCompass="true"
+                    zoomLevel="12"
+                    showUserLocation="false"
+                    disableZoom="false"
+                    disableRotation="false"
+                    disableScroll="false"
+                    disableTilt="false"
+                    @mapReady="onMapReady($event)"
+                    height=50%
+                    width=*>
+                </Mapbox>
+                
+                <!-- <RadDataForm :source="form" /> -->
+                <PickerField hint="Radius" :items="pickerItems" ref="apiPicker"></PickerField>
+                
+                <StackLayout orientation="horizontal">
+                <Button text="Start Game" width="100%" height="30%"
+                    backgroundColor="#5EB0E5" marginTop="20" textAlignment="center"
+                    color="white" fontSize="20" fontWeight="bold"
+                    borderRadius="20" @tap="$goto('Game')" />
+                    </StackLayout>
+        </StackLayout>
+    </Page>
+</template>
+
+
+<script>
+    import * as utils from "utils/utils";
+    import Vue from "nativescript-vue";
+    import RadDataForm from "nativescript-ui-dataform/vue";
+    import PickerField from 'nativescript-picker/vue';
+    import axios from 'axios'
+ 
+    Vue.use(PickerField);
+    Vue.use(RadDataForm);
+
+    export default {
+        methods: {
+            onViewButtonClick() {
+                let picker = this.$refs.apiPicker.nativeView;
+                console.log('picker', picker.selectedValue)
+                // console.log(form.To, form.From)
+                let address = '2316 odin street'
+
+                // axios({
+                // method: 'GET',
+                // url: `https://api.opencagedata.com/geocode/v1/json?key=4b1276c879054864aeed6ec75901de2b&q=611 N Miro Street&pretty=1&no_annotations=1`,
+                // })
+                // .then(function (response) {
+                //     console.log('response', response);
+                // });
+            },
+            onMapReady(args) {
+                args.map.addMarkers([
+                    {
+                        lat: 29.9643504,
+                        lng: -90.0816426,
+                        title: "Tracy, CA",
+                        subtitle: "Home of The Polyglot Developer!",
+                        onCalloutTap: () => {
+                            utils.openUrl("https://www.thepolyglotdeveloper.com");
+                        }
+                    },
+                    {
+                        lat: 30.0146884,
+                        lng: -90.0577187, 
+                        title: "Tracy, CA",
+                        subtitle: "Home of The Polyglot Developer!",
+                        onCalloutTap: () => {
+                            utils.openUrl("https://www.thepolyglotdeveloper.com");
+                        }
+                    },
+                    {
+                        lat: 29.6643504,
+                        lng: -90.0816426,
+                        title: "Tracy, CA",
+                        subtitle: "Home of The Polyglot Developer!",
+                        onCalloutTap: () => {
+                            utils.openUrl("https://www.thepolyglotdeveloper.com");
+                        }
+                    },
+                    {
+                        lat: 30.0643504,
+                        lng: -90.0816426,
+                        title: "Tracy, CA",
+                        subtitle: "Home of The Polyglot Developer!",
+                        onCalloutTap: () => {
+                            utils.openUrl("https://www.thepolyglotdeveloper.com");
+                        }
+                    }
+                ]);
+            }
+            
+        },
+        data() {
+            return {
+                pickerItems: [
+                    15, 35, 55
+                ],
+                // form: {
+                //     From: "X",
+                //     To: "Y",
+                // }
+            };
+        }
+    };
+</script>
+
+<style scoped>
+    .home-panel {
+        vertical-align: center;
+        font-size: 20;
+        margin: 15;
+    }
+
+    .description-label {
+        margin-bottom: 15;
+    }
+</style>
--- app/components/views/Lobby.vue
+++ app/components/views/Lobby.vue
@@ -1,26 +1,21 @@
 <template>
     <Page class="page">
-        <ActionBar title="Home">
-            
-                <Button height="40" width="40" borderRadius="30"
-                    text="Profile" color="white"
-                    backgroundColor="#eb8100" ios.position="right"
-                    android.position="popup" @tap="$goto('Profile')" />  
+        <ActionBar title="Lobby">
         </ActionBar> 
 
         
         <ScrollView>
-            <StackLayout orientation="horizontal" width="*" height="*"
+            <StackLayout orientation="vertical" width="*" height="*"
                 backgroundColor="white">
 
-                <Button text="Create Game" width="50%" height="20%"
+                <Button text="Join Makiyah's Game" width="80%" height="20%"
                     backgroundColor="#5EB0E5" marginTop="20" textAlignment="center"
                     color="white" fontSize="20" fontWeight="bold"
-                    borderRadius="30" @tap="$goto('CreateGame')" />
-                <Button text="Join Game" width="50%" height="20%"
+                    borderRadius="30" @tap="$goto('Placeholder')" />
+                <Button text="Join Wilson's Game" width="80%" height="20%"
                 backgroundColor="#5EB0E5" marginTop="20" textAlignment="center"
                 color="white" fontSize="20" fontWeight="bold"
-                borderRadius="30" @tap="$goto('Lobby')" />
+                borderRadius="30" @tap="$goto('Placeholder')" />
             </StackLayout>
         </ScrollView>
     </Page>
