6c527483d1c7e91931c4704a5ca85028e159ed25
AS Wilson <alexander.wilson@ncf.edu>
2019-08-14 15:08:40 -0500

    Create new vue for games in lobby

---
+++ app/components/views/LobbyGame.vue
@@ -0,0 +1,36 @@
+<template>
+    <ScrollView>
+      <StackLayout orientation="vertical" width="*" height="*" backgroundColor="white">
+        <ListView for="game in games" style="height:1250px">
+          <v-template>
+            <Button
+              :text="game.code"
+              @tap="handleJoin(game)"
+              width="80%"
+              height="20%"
+              backgroundColor="#5EB0E5"
+              marginTop="20"
+              textAlignment="center"
+              color="white"
+              fontSize="20"
+              fontWeight="bold"
+              borderRadius="30"
+            />
+          </v-template>
+        </ListView>
+      </StackLayout>
+    </ScrollView>
+</template>
+
+<script>
+export default {
+  name: 'LobbyGame',
+  props: {
+    games: Array,
+    handleJoin: Function,
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
