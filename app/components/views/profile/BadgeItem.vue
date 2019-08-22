faabf6c05933f17985e6b88fffd0ff18ec0434c6
AS Wilson <alexander.wilson@ncf.edu>
2019-08-21 21:12:36 -0500

    Create a badge item view

---
+++ app/components/views/profile/BadgeItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <GridLayout rows="*" columns="auto">
+    <img row="0" width="100%" :src="`~/assets/badges/${badge.name}.png`" />
+    <Label row="1" width="100%" :text="badge.description" />
+  </GridLayout>
+</template>
+
+<script>
+export default {
+  name: 'BadgeItem',
+  props: {
+    badge: Object,
+    index: Number,
+  }
+}
+</script>
