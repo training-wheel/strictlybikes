6db8048b92505897530718c683a0bc6784319444
AS Wilson <alexander.wilson@ncf.edu>
2019-08-21 21:12:09 -0500

    Set profile image to ratio height

--- app/components/views/profile/Profile.vue
+++ app/components/views/profile/Profile.vue
@@ -12,7 +12,7 @@
         <StackLayout>
           <Label :text="username" color="#ff9933" flexGrow=".3" fontWeight="bold" fontSize="24" horizontalAlignment="center" />
           <FlexboxLayout justifyContent="space-around" >
-            <Image :src="imageUrl" height="20%" width="auto" borderRadius="90" />
+            <Image :src="imageUrl" height="10%" width="auto" borderRadius="90" />
             <PlayerStats fontSize="15" alignSelf="center" v-if="userMetrics.length > 0" :userMetrics="userMetrics" />
           </FlexboxLayout>
           <Label text="BADGES" fontWeight="bold" fontSize="24" textAlignment="center" />
